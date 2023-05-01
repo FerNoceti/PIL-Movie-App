afe77e80a7c81f954b3c34425c927633301e1471
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-05-01 19:13:52 -0300

    vote average color
    must check the yellow

--- app/src/main/java/com/pil/movieapp/adapter/MovieAdapter.kt
+++ app/src/main/java/com/pil/movieapp/adapter/MovieAdapter.kt
@@ -28,10 +28,22 @@ class MovieAdapter(private val movies: List<Movie>) : RecyclerView.Adapter<Movie
             binding.id.text = itemView.context.getString(R.string.card_id, movie.id.toString())
             binding.title.text = itemView.context.getString(R.string.card_title, movie.title)
             binding.overview.text = itemView.context.getString(R.string.card_overview, movie.overview)
+            binding.date.text = itemView.context.getString(R.string.card_release_date, movie.releaseDate)
+            binding.voteAverage.text = itemView.context.getString(R.string.card_vote_average, movie.voteAverage.toString())
+            binding.voteAverage.setTextColor(calculateColor(movie.voteAverage))
 
             Glide.with(itemView.context)
                 .load(itemView.context.getString(R.string.card_poster_path, movie.posterPath))
                 .into(binding.image)
+        }
+
+        private fun calculateColor(voteCount: Float): Int {
+            return when {
+                voteCount < 4 -> itemView.context.getColor(R.color.red)
+                voteCount < 6 -> itemView.context.getColor(R.color.orange)
+                voteCount < 7 -> itemView.context.getColor(R.color.yellow)
+                else -> itemView.context.getColor(R.color.green)
+            }
         }
     }
 }
