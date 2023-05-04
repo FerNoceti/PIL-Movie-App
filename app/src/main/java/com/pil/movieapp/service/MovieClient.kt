a9477a30ecd329693ed32a0bc088851b350d7974
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-05-04 19:18:02 -0300

    Update MovieClient.kt

--- app/src/main/java/com/pil/movieapp/service/MovieClient.kt
+++ app/src/main/java/com/pil/movieapp/service/MovieClient.kt
@@ -7,5 +7,5 @@ import retrofit2.http.Query
 
 interface MovieClient {
     @GET("movie/popular")
-    fun getData(@Query("page") page: Int? = null): Call<MovieList>
+    fun getData(): Call<MovieList>
 }
