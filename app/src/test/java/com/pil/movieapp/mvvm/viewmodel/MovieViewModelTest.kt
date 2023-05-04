d09af8a7d124518fcadfb3a62d3c8683839ea9a8
Fernando José Noceti <fer.j.noceti@gmail.com>
2023-05-04 19:14:36 -0300

    test package correction

--- app/src/test/java/com/pil/movieapp/mvvm/viewmodel/MovieViewModelTest.kt
+++ app/src/test/java/com/pil/movieapp/mvvm/viewmodel/MovieViewModelTest.kt
@@ -1,22 +1,26 @@
-package com.pil.movieapp.mvvm
+package com.pil.movieapp.mvvm.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.pil.movieapp.database.MovieDataBase
 import com.pil.movieapp.mvvm.contract.MenuContract
+import com.pil.movieapp.mvvm.contract.MovieContract
+import com.pil.movieapp.mvvm.model.MovieModel
 import com.pil.movieapp.mvvm.viewmodel.MenuViewModel
+import com.pil.movieapp.mvvm.viewmodel.MovieViewModel
+import com.pil.movieapp.service.MovieService
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TestRule
 
-class MenuViewModelTest {
+class MovieViewModelTest {
 
     @get:Rule
     var rule: TestRule = InstantTaskExecutorRule()
 
-    private lateinit var viewModel: MenuContract.ViewModel
+    private lateinit var viewModel: MovieContract.ViewModel
 
     @Before
     fun setUp(){
-        viewModel = MenuViewModel()
     }
 }
